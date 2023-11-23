ea5451ac888a32c43347f57169baa2591c0f41d4
Anatolii <anatolii.mart6@gmail.com>
2023-11-23 11:14:25 +0100

    Добавлено модальное окно

--- script.js
+++ script.js
@@ -12,20 +12,37 @@ setInterval(() => {
 }, 200);
 
 ///////////////////////////////////////////////////////////////
-// Modal dialog
+// Modal dialogs
 const btnOptions = document.getElementById("options");
-const clsModal = document.getElementById("close-btn");
+const btnInfo = document.getElementById("info");
+const btnLegacy = document.getElementById("legacy");
+const btnStats = document.getElementById("stats");
+const closeOptions = document.getElementById("close-btn");
 
-btnOptions.addEventListener("click", openModal);
-clsModal.addEventListener("click", closeModal);
+btnOptions.onclick = () => { openClose("options_modal"); };
 
-function closeModal() {
-    console.log('clossed modal');
-  document.getElementById("options_modal").style.display = "none";
+btnInfo.onclick = () => { openClose("info_modal"); };
+
+btnLegacy.onclick = () => { openClose("legacy_modal"); };
+
+btnStats.onclick = () => { openClose("stats_modal"); };
+
+
+function openClose(idName) {
+  openModal(idName);
+  const closing = document.getElementById(idName).querySelector(`#close-btn`);
+  closing.onclick = () => {
+    closeModal(idName);
+  };
+}
+
+function closeModal(idName) {
+  document.getElementById(idName).style.display = "none";
   document.getElementById("overlay").style.display = "none";
 }
-function openModal() {
-    console.log('opened modal');
-  document.getElementById("options_modal").style.display = "flex";
+
+
+function openModal(idName) {
+  document.getElementById(idName).style.display = "flex";
   document.getElementById("overlay").style.display = "block";
 }
