8511c3657d02b6e350df43f45d7f26048b5b50c8
Waken <votrevincentwaken@gmail.com>
2023-11-27 21:07:17 +0200

    Added State.js which stores the game state, also changed the scriptOD structure by importing state variables and updating them on a click event. Also in saving.js in loadLocal importing the state and initializing the game state variables

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-const ctx = canvas.getContext("2d");  // получаем контекст канваса, чтоб говорить полотну где и что рисовать
-let img = document.getElementById('img');
+const ctx = canvas.getContext("2d"); // получаем контекст канваса, чтоб говорить полотну где и что рисовать
+let img = document.getElementById("img");
 
 // Двигающийся фон
 let x = 0;
 let y = 0;
 setInterval(() => {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    y+=1;
-    ctx.drawImage(img, x, y, 10, 5);
-    if(y === 120) y = 0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  y += 1;
+  ctx.drawImage(img, x, y, 10, 5);
+  if (y === 120) y = 0;
 }, 200);
 
 ///////////////////////////////////////////////////////////////
+
+// Modal dialogs
+const btnOptions = document.getElementById("options");
+const btnInfo = document.getElementById("info");
+const btnLegacy = document.getElementById("legacy");
+const btnStats = document.getElementById("stats");
+
+btnOptions.onclick = () => { openClose("options_modal"); };
+
+btnInfo.onclick = () => { openClose("info_modal"); };
+
+btnLegacy.onclick = () => { openClose("legacy_modal"); };
+
+btnStats.onclick = () => { openClose("stats_modal"); };
+
+
+function openClose(idName) {
+  openModal(idName);
+  const closing = document.getElementById(idName).querySelector(`#close-btn`);
+  closing.onclick = () => {
+    closeModal(idName);
+  };
+}
+
+function closeModal(idName) {
+  document.getElementById(idName).style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+
+function openModal(idName) {
+  document.getElementById(idName).style.display = "flex";
+  document.getElementById("overlay").style.display = "block";
+}
 function increment() {
     
     let spanElement = document.getElementById('mySpan1');
@@ -22,3 +56,5 @@ function increment() {
 
     spanElement.textContent = newScore;
   }
+
+  
--- scriptOD.js
+++ scriptOD.js
@@ -1,43 +1,76 @@
+import { state } from './State.js';
 
 
  function incrementD() {
     
-    let spanElement = document.getElementById('mySpan1');
+    let spanElement = document.getElementById('emeraldsCounter');
     console.log(spanElement.textContent);
     let currentScore = parseInt(spanElement.textContent);
-     console.log(currentScore);
+     //console.log(currentScore);
     let newScore = currentScore + 1;
-
+    state.score = newScore;
     spanElement.textContent = newScore;
-    
-   
-
   }
 
 
   document.getElementById('bigButton').addEventListener('click', function() 
   {
-  playClickSound();
- 
+
+    incrementD();
+    playClickSound();
+    createTransformAndRemoveImage()
   });
 
    function playClickSound() {
     
    let audio = new Audio("sound/udar-po-metallicheskomu-predmetu.mp3"); // Указывает путь путь к звуковому файлу
    audio.play();
+   audio.volume = 0.5;
 }
 
+function createTransformAndRemoveImage() {
+  // Создание элемента <img>
+  let img = document.createElement('img');
+   img.id = 'dinamicImg';
+  // Установка атрибута src (путь к изображению)
+  img.src = 'img/emerald.png';
 
-document.addEventListener('DOMContentLoaded', function () {
-  var myElement = document.getElementById('imgButton');
+  // Добавление класса для применения стилей трансформации
+  img.classList.add('transformed');
 
-  myElement.addEventListener('click', function () {
-    // Добавляем класс "clicked" для анимации тени
-    myElement.classList.add('clicked');
+  // Добавление элемента <img> к body (вы можете выбрать другой контейнер)
+  let left_div = document.getElementById('infoLeft');
+ left_div.appendChild(img);
 
-    // Убираем класс "clicked" через 0.5 секунды для сброса анимации
+  // Выполнение трансформации (в данном случае, добавление класса)
+  // Можете изменить этот блок в соответствии с вашими требованиями для трансформации
+  setTimeout(function () {
+    // Время для отображения изображения перед трансформацией
+
+    // Удаление класса (можете изменить это на другие операции трансформации)
+    img.classList.remove('transformed');
+
+    // Удаление изображения после завершения трансформации
     setTimeout(function () {
-      myElement.classList.remove('clicked');
-    }, 500);
-  });
+      left_div.removeChild(img);
+    }, 500); // Например, через 1 секунду после трансформации
+  }, 0); // Передача 0 миллисекунд для выполнения в следующем доступном кадре анимации
+}
+
+//#region  фоновая музыка
+document.addEventListener('click', function () {
+  let backgroundMusic = document.getElementById('backgroundMusic');
+  let backgroundMusic1 = document.getElementById('backgroundMusic1')
+  // Воспроизведение музыки
+  
+  backgroundMusic.play();
+  backgroundMusic1.play();
+  // Пауза музыки
+  // backgroundMusic.pause();
+
+  // Изменение громкости (значение от 0.0 до 1.0)
+  backgroundMusic.volume = 0.3;
+  backgroundMusic1.volume = 0.3;
+  
 });
+//#endregion
