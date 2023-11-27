0ec817f195aac7f0cc1a8bf0234b771abbbbfc3e
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2023-11-27 20:00:44 +0300

    !

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const ctx = canvas.getContext("2d"); // получаем контекст канваса, чтоб говорить полотну где и что рисовать
-let img = document.getElementById("img");
-
-// Двигающийся фон
-let x = 0;
-let y = 0;
-setInterval(() => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  y += 1;
-  ctx.drawImage(img, x, y, 10, 5);
-  if (y === 120) y = 0;
-}, 200);
-
-///////////////////////////////////////////////////////////////
-
 // Modal dialogs
 const btnOptions = document.getElementById("options");
 const btnInfo = document.getElementById("info");
