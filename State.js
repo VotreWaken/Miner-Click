629223cad17ba6476a9064bb84855bd092481992
Waken <votrevincentwaken@gmail.com>
2023-11-27 21:21:53 +0200

    Added State.js which stores the game state, also changed the scriptOD structure by importing state variables and updating them on a click event. Also in saving.js in loadLocal importing the state and initializing the game state variables

---
+++ State.js
@@ -0,0 +1,12 @@
+export const state = 
+{
+    score: 0,
+    cursors: 0,
+    pickaxes: 0,
+    dynamites: 0,
+    items: {
+      'Cursor': { cost: 10, income: 1 },
+      'Pickaxe': { cost: 50, income: 5 },
+      'Dynamite': { cost: 100, income: 10 }
+    }
+};
